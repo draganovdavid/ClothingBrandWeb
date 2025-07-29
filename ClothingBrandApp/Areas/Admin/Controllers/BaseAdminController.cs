842b334365943faaade15ff05077b4bac620d8e6
David Draganov <daviddraganov891@gmail.com>
2025-07-29 20:32:05 +0300

    Introduce Admin area.

---
+++ ClothingBrandApp/Areas/Admin/Controllers/BaseAdminController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using static ClothingBrandApp.GCommon.ApplicationConstants;
+
+namespace ClothingBrandApp.Web.Areas.Admin.Controllers
+{
+    [Area(AdminRoleName)]
+    [Authorize(Roles = AdminRoleName)]
+    public abstract class BaseAdminController : Controller
+    {
+        private bool IsUserAuthenticated()
+        {
+            bool retRes = false;
+            if (this.User.Identity != null)
+            {
+                retRes = this.User.Identity.IsAuthenticated;
+            }
+
+            return retRes;
+        }
+
+        protected string? GetUserId()
+        {
+            string? userId = null;
+            if (this.IsUserAuthenticated())
+            {
+                userId = this.User
+                    .FindFirstValue(ClaimTypes.NameIdentifier);
+            }
+
+            return userId;
+        }
+    }
+}
